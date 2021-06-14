f27a7f2b94cc30fb6e9fa4be054a75f63073e8cc
AkhatorEnosa <aosakhogba@gmail.com>
2021-06-14 15:35:49 +0100

    minor layout and functionality changes

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-document.querySelector(".option").addEventListener('click', function (e) {
-  e.target.cssText = "{background: black; font-size: 2rem;}";
-  console.log("clicked")
-})
+var ul = document.querySelector("#man");
+var manValue = "";
+
+function getButton(e) {
+  if(e.target && !e.target.classList.contains("selected")){
+    e.target.classList.add("selected");
+    manValue = e.target.value;
+    var notSelected = document.querySelectorAll("button.man:not(.selected)");
+    notSelected.forEach(x => {
+      x.style.cssText = "opacity: 0.5; transform: scale(0.5)";
+      x.disabled = true;
+    });
+
+    console.log(manValue);
+	}else{
+		e.target.classList.remove("selected");
+    manValue = "";
+    var notSelected = document.querySelectorAll("button.man");
+    notSelected.forEach(x => {
+      x.style.cssText = "opacity: 1; transform: scale(1)";
+      x.disabled = false;
+    });
+    console.log(manValue)
+	}
+}
+
+ul.addEventListener('click', getButton)
